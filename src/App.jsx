4006e3c1573505903063ee4c8ed056cb937bc3a0
Babz-G <barbaramgaynor@gmail.com>
2025-12-03 16:40:09 -0500

    initial push - toggle & clock

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import Toggle from "./Components/Toggle.jsx";
+import Clock from "./Components/Clock.jsx";
+import "react-clock/dist/Clock.css";
+
+function App() {
+  return (
+    <>
+      Babz' Project
+      <Toggle />
+      <Clock />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/Clock.jsx
@@ -0,0 +1,46 @@
+// import { useEffect, useState } from "react";
+// import Clock from "react-clock";
+
+// function MyApp() {
+//   const [value, setValue] = useState(new Date());
+
+//   useEffect(() => {
+//     const interval = setInterval(() => setValue(new Date()), 1000);
+
+//     return () => {
+//       clearInterval(interval);
+//     };
+//   }, []);
+
+//   return (
+//     <div>
+//       <p>Current time:</p>
+//       <Clock value={value} />
+//     </div>
+//   );
+// }
+
+import { useEffect, useState } from "react";
+import Clock from "react-clock";
+import "react-clock/dist/Clock.css";
+
+function ClockComponent() {
+  const [value, setValue] = useState(new Date());
+
+  useEffect(() => {
+    const interval = setInterval(() => setValue(new Date()), 1000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
+  return (
+    <div>
+      <p>Current time:</p>
+      <Clock value={value} />
+    </div>
+  );
+}
+
+export default ClockComponent;
